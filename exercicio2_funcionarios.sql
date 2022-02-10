db8a3733b026675e02d3d034ea768b89decae722
Fabio Shreiner <fshreiner21@gmail.com>
2022-02-10 12:34:14 -0300

    avançando

---
+++ exercicio2_funcionarios.sql
@@ -0,0 +1,18 @@
+/* Considerando o que vc aprendeu sobre performance, resolva os exercicios abaixo */
+
+USE EXERCICIO;
+
+SELECT COUNT(*) FROM FUNCIONARIOS;
+
+--Traga os funcionarios que trabalhem
+--no departamento de filmes OU no
+--departamento de roupas
+
+
+--O gestor de marketing pediu a lista das funcionarias (AS) = FEMININO que trabalhem no departamento 
+--de filmes ou no departamento lar. Ele necessita enviar um email para as colaboradoras
+--desses dois setores. OR +  AND *
+
+
+--Traga os funcionarios do sexo masculino
+--ou os funcionarios que trabalhem no setor Jardim
